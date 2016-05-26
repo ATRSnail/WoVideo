8ae11e9891a0d885168c554044780c3c4fdb1e12
leov1 <tiantang3241@163.com>
2016-05-26 17:51:03 +0800

    Initail Commit

---
+++ app/src/main/java/com/lt/hm/wovideo/AppStart.java
@@ -0,0 +1,32 @@
+package com.lt.hm.wovideo;
+
+import android.os.Bundle;
+
+import com.lt.hm.wovideo.base.BaseActivity;
+
+/**
+ * @author leonardo
+ * @version 1.0
+ * @create_date 16/5/26
+ */
+public class AppStart extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return super.getLayoutId();
+    }
+
+    @Override
+    protected void init(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void initViews() {
+
+    }
+
+    @Override
+    public void initDatas() {
+
+    }
+}

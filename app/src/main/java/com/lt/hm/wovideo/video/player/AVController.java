c707d3bbad25c44d59cc1541fd751e8a8ae72a20
KECB <406761423@qq.com>
2016-07-20 16:24:35 +0800

    Make back button easy to touch.

--- app/src/main/java/com/lt/hm/wovideo/video/player/AVController.java
+++ app/src/main/java/com/lt/hm/wovideo/video/player/AVController.java
@@ -68,7 +68,7 @@ public class AVController extends FrameLayout implements AVPlayerGestureListener
   private ImageButton         mNextButton;
   private ImageButton         mPrevButton;
   private ImageButton         mFullscreenButton;
-  private ImageButton         mBackButton;
+  private ImageView         mBackButton;
   private ImageButton         mSendBulletButton;
   private QualityPopWindow    mQualityPopWindow;
   private Handler             mHandler = new MessageHandler(this);
@@ -228,7 +228,7 @@ public class AVController extends FrameLayout implements AVPlayerGestureListener
       mBulletSwitch.setOnCheckedChangeListener(mBulletSwitchListener);
     }
 
-    mBackButton = (ImageButton) v.findViewById(R.id.back);
+    mBackButton = (ImageView) v.findViewById(R.id.back);
     if (mBackButton != null) {
       mBackButton.setOnClickListener(mBackListener);
     }
